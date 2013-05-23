3859ccb215082deff9d771ac63a7aa0b7e5787f8
João Lopes <joaopcloeps@gmail.com>
2013-05-23 14:34:49 +0100

    Templates (Header, Footer) e CSS
    Criados mas ainda nao utilizados em lugar nenhum.

---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Frown - <?php echo $title ?></title>
+    <link rel="stylesheet" href="<?php echo base_url();?>application/views/css/style.css">
+</head>
+<body>
+<div id="body">
